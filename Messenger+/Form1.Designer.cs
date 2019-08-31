470ceaa7f298e6a26253f30c8b9e2fdf772f12c0
wulinworks <wulinworks@gmail.com>
2019-08-31 16:09:09 +0800

    Initial commit

---
+++ Messenger+/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Messenger_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.shiftTimer = new System.Windows.Forms.Timer(this.components);
+            this.title = new System.Windows.Forms.PictureBox();
+            this.dragTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.title)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // shiftTimer
+            // 
+            this.shiftTimer.Interval = 200;
+            this.shiftTimer.Tick += new System.EventHandler(this.ShiftTimer_Tick);
+            // 
+            // title
+            // 
+            this.title.Image = ((System.Drawing.Image)(resources.GetObject("title.Image")));
+            this.title.Location = new System.Drawing.Point(0, 0);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(506, 20);
+            this.title.TabIndex = 0;
+            this.title.TabStop = false;
+            // 
+            // dragTimer
+            // 
+            this.dragTimer.Interval = 10;
+            this.dragTimer.Tick += new System.EventHandler(this.DragTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(526, 573);
+            this.Controls.Add(this.title);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.Text = "Messenger+";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.title)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer shiftTimer;
+        private System.Windows.Forms.PictureBox title;
+        private System.Windows.Forms.Timer dragTimer;
+    }
+}
+
---
+++ Messenger+/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Reflection;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Resources;
+
+namespace Messenger_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitBrowser();
+            FormClosing += new FormClosingEventHandler(Form1_Closing);
+            title.MouseDown += new MouseEventHandler(title_MouseDown);
+            title.MouseUp += new MouseEventHandler(title_MouseUp);
+        }
+
+        public ChromiumWebBrowser browser;
+        public void InitBrowser()
+        {
+            using (CefSettings settings = new CefSettings())
+            {
+                settings.CachePath = Path.GetTempPath() + @"\CEF";
+                Cef.Initialize(settings);
+            }
+
+            browser = new ChromiumWebBrowser("https://messenger.com");
+            Controls.Add(browser);
+            browser.Dock = DockStyle.None;
+            browser.Width = 520;
+            browser.Height = 550;
+            browser.Location = new Point(3, 20);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = Application.StartupPath + @"\location.dat";
+
+            if (File.Exists(path))
+            {
+                using (StreamReader f = File.OpenText(path))
+                {
+                    string s = f.ReadLine();
+                    f.Close();
+
+                    int x = int.Parse(s.Split(',')[0]);
+                    int y = int.Parse(s.Split(',')[1]);
+                    Location = new Point(x, y);
+                }
+            }
+
+
+            GlobalKeyboardHook gHook = new GlobalKeyboardHook();
+            gHook.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            foreach (Keys key in Enum.GetValues(typeof(Keys))) gHook.HookedKeys.Add(key);
+
+            gHook.hook();
+        }
+
+        public bool shift = false;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape && this.ContainsFocus) Application.Exit();
+            else if (e.KeyCode == Keys.LShiftKey || e.KeyCode == Keys.RShiftKey)
+            {
+                shift = true;
+                shiftTimer.Enabled = true;
+            }
+            else if (shift)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Tab:
+                        if (Visible == true) Visible = false;
+                        else Visible = true;
+                        break;
+                }
+            }
+        }
+
+        private void ShiftTimer_Tick(object sender, EventArgs e)
+        {
+            shift = false;
+        }
+
+        public int offsetX, offsetY;
+        private void DragTimer_Tick(object sender, EventArgs e)
+        {
+            Location = new Point(Cursor.Position.X - offsetX, Cursor.Position.Y - offsetY);
+        }
+
+        private void title_MouseDown(object sender, MouseEventArgs e)
+        {
+            offsetX = (Cursor.Position.X - Location.X);
+            offsetY = (Cursor.Position.Y - Location.Y);
+            dragTimer.Enabled = true;
+        }
+        
+        private void title_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragTimer.Enabled = false;
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            string path = Application.StartupPath + @"\location.dat";
+
+            if (File.Exists(path)) File.Delete(path);
+            using (StreamWriter f = File.CreateText(path))
+            {
+                f.WriteLine(Location.X + "," + Location.Y);
+                f.Close();
+            }
+        }
+    }
+}
